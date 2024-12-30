759c97503d7dba4d894e6d18c908d6a6787b9d78
cyrustse <tsechunhong@gmail.com>
2024-12-30 16:52:07 +0800

    Redeploy

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/cyrustse.github.io',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
